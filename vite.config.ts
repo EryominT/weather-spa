eb9fc2107bb5c306dc4f3a7b3835b6a1585d1ee6
EryominT <eryomin.t@mail.com>
2025-08-25 13:02:39 +0700

    fix setting github-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
     vue(),
     vueDevTools(),
@@ -15,5 +15,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: import.meta.env.PROD ? '/weather-spa/' : '/',
-});
+  base: mode === 'production' ? '/weather-spa/' : '/',
+}));
